02693e6482e922beca5a5197f468fc4ae405f37f
Vector1331 <tpgus4796@gmail.com>
2022-04-21 14:26:08 +0900

    Initial setting commit

---
+++ ae_Backend/src/main/java/com/ae/ae_Backend/AeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ae.ae_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AeBackendApplication.class, args);
+	}
+
+}
---
+++ ae_Backend/src/test/java/com/ae/ae_Backend/AeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ae.ae_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
